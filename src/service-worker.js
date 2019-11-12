b1d1306c2e7bdb6efa54aace622992080f715bf4
usmysa <b1012206@gamil.com>
2019-11-12 10:32:31 +0900

    #4 service workerの設定

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,1 +1,17 @@
-// ここにService Worker処理の内容を書く
+workbox.core.setCacheNameDetails({prefix: 'goggl'});
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,12 @@
 module.exports = {
+  devServer: {
+    host: 'localhost'
+  },
   pwa: {
-    // configure the workbox plugin
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
-      // swSrc is required in InjectManifest mode.
       swSrc: 'src/service-worker.js',
       swDest: 'service-worker.js'
-
-      // other options here...
     }
   }
 }
