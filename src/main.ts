6214ade0003071421d2f33f51ea2e43080dc1b52
usmysa <b1012206@gamil.com>
2019-11-24 17:31:32 +0900

    main.tsの修正

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import firestore from './firestore';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  data: {
+    db: firestore,
+  },
   render: h => h(App),
 }).$mount('#app');
