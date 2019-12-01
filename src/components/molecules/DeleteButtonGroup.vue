6c0ba21806118e88e7c773cfb63cce16f0d879f6
usmysa <b1012206@gmail.com>
2019-12-01 17:14:21 +0900

    #13 DeleteButtonGroupの実装

--- src/components/molecules/DeleteButtonGroup.vue
+++ src/components/molecules/DeleteButtonGroup.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="DeleteButtonGroup">
-    DeleteButtonGroup
+    <ApplyButton>Delete</ApplyButton>
+    <CancelButton>Cancel</CancelButton>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import ApplyButton from '~/atoms/ApplyButton.vue';
+import CancelButton from '~/atoms/CancelButton.vue';
 
-@Component
+@Component({
+  components: {
+    ApplyButton,
+    CancelButton,
+  },
+})
 export default class DeleteButtonGroup extends Vue {
 }
 </script>
