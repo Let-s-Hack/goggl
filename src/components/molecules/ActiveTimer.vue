25a1aaed60786d36b64613fd368738c7fa6a1c63
usmysa <b1012206@gmail.com>
2019-12-02 21:16:50 +0900

    #13 レビュー修正

--- src/components/molecules/ActiveTimer.vue
+++ src/components/molecules/ActiveTimer.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="ActiveTimer">
-    <div class="ActiveTimer_Label">
-      <div class="ActiveTimer_Time">1:43:35</div>
-      <div class="ActiveTimer_TitleGroup">
-        <!-- TODO: 文字数が多い場合のアニメーションの実装 -->
-        <span class="ActiveTimer_Title">goggl | 静的コーディング</span>
-        <span
-          class="ActiveTimer_Project"
-          :style="{ borderColor: '#E30909', color: '#E30909' }"
-        >
-          goggl
-        </span>
-      </div>
+    <div class="ActiveTimer_Time">1:43:35</div>
+    <div class="ActiveTimer_TitleGroup">
+      <!-- TODO: 文字数が多い場合のアニメーションの実装 -->
+      <span class="ActiveTimer_Title">gogl | 静的コーディング</span>
+      <span
+        class="ActiveTimer_Project"
+        :style="{ borderColor: '#E30909', color: '#E30909' }"
+      >
+        goggl
+      </span>
     </div>
     <TimerStopButton class="ActiveTimer_Button" />
   </div>
@@ -32,37 +30,32 @@ export default class ActiveTimer extends Vue {
 
 <style lang="scss" scoped>
 .ActiveTimer {
+  flex: 1;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
   height: 72px;
+  padding: 16px 48px 12px 44px;
   background: #FFF;
   border-radius: 8px 8px 0 0;
   border-bottom: 1px solid #D8D8D8;
   box-shadow: 0 -2px 8px 0px rgba(#424147, 0.17);
   box-sizing: border-box;
-
-  &_Label {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    height: 100%;
-    margin-left: 45px;
-    text-align: center;
-    font-size: 1.4rem;
-    overflow: hidden;
-  }
+  font-size: 1.4rem;
+  text-align: center;
 
   &_Time {
-    font-weight: bold;
+    margin-bottom: auto;
+    font-weight: 500;
     letter-spacing: 0.1rem;
-    margin-bottom: 8px;
   }
 
   &_TitleGroup {
     position: relative;
     display: flex;
     justify-content: center;
-    text-align: center;
+    padding-bottom: 4px;
+    overflow: hidden;
 
     &::before {
       position: absolute;
@@ -71,7 +64,7 @@ export default class ActiveTimer extends Vue {
       display: block;
       width: 16px;
       height: 110%;
-      background: linear-gradient(to right, #F9FAFC, rgba(#F9FAFC, 0));
+      background: linear-gradient(to right, #FFF, rgba(#FFF, 0));
       content: '';
     }
 
@@ -82,19 +75,17 @@ export default class ActiveTimer extends Vue {
       display: block;
       width: 16px;
       height: 110%;
-      background: linear-gradient(to left, #F9FAFC, rgba(#F9FAFC, 0));
+      background: linear-gradient(to left, #FFF, rgba(#FFF, 0));
       content: '';
     }
   }
 
   &_Title {
     white-space: nowrap;
-    text-overflow: clip;
   }
 
   &_Project {
     white-space: nowrap;
-    text-overflow: clip;
 
     &::before {
       display: inline-block;
@@ -108,8 +99,9 @@ export default class ActiveTimer extends Vue {
   }
 
   &_Button {
-    margin: auto;
-    margin-right: 11px;
+    position: absolute;
+    right: 0;
+    margin-right: 12px;
   }
 }
 </style>
