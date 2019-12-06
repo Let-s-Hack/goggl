18a1f315442105c5fbb1a6a77f8dbffd22aed1dc
usmysa <b1012206@gmail.com>
2019-12-06 21:46:03 +0900

    #11 ヘッダーの作成

--- src/components/organisms/ReportsHeader.vue
+++ src/components/organisms/ReportsHeader.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="Reportsheader">
-    Reportsheader
-    <slot />
+  <div class="ReportsHeader">
+    <h1 class="ReportsHeader_Title">This week</h1>
   </div>
 </template>
 
@@ -12,3 +11,37 @@ import { Component, Vue } from 'vue-property-decorator';
 export default class Reportsheader extends Vue {
 }
 </script>
+
+<style lang="scss" scoped>
+.ReportsHeader {
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  top: 0;
+  width: 100%;
+  height: 44px;
+  padding: 8px 0;
+  border-bottom: 1px solid #A9A9AC;
+  background-color: #FFF;
+  box-sizing: border-box;
+
+  &_Title {
+    position: relative;
+    text-align: center;
+    font-size: 1.3rem;
+    letter-spacing: 0.1rem;
+
+    &::after {
+      content: '';
+      width: 0;
+      height: 0;
+      position: absolute;
+      top: 50%;
+      margin-left: 3px;
+      border: 2px solid transparent;
+      border-top: 3px solid #000;
+    }
+  }
+}
+</style>
