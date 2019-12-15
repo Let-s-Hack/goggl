d17396e3f9f81bb7571ffc4c65e63f37b021733f
Homma <ab13891012.gmail.com>
2019-12-15 13:00:33 +0900

    #39 publicディレクトリへのパスを設定

--- src/components/atoms/BaseIcon.vue
+++ src/components/atoms/BaseIcon.vue
@@ -8,20 +8,20 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 // 相対パスである必要がある
 // TODO: SVGを一つずつimportしているので、一括でimportしたい。参考：https://github.com/nicooprat/icon.vue/blob/master/src/components/Icon.vue
-import IconAdd from '../../../public/img/icons/add.svg';
-import IconArrow from '../../../public/img/icons/arrow.svg';
-import IconCalendar from '../../../public/img/icons/calendar.svg';
-import IconCheckCircle from '../../../public/img/icons/check-circle.svg';
-import IconClose from '../../../public/img/icons/close.svg';
-import IconGraph from '../../../public/img/icons/graph.svg';
-import IconSearch from '../../../public/img/icons/search.svg';
-import IconSetting from '../../../public/img/icons/setting.svg';
-import IconSpinner from '../../../public/img/icons/spinner.svg';
-import IconTag from '../../../public/img/icons/tag.svg';
-import IconTime from '../../../public/img/icons/time.svg';
-import IconTimer from '../../../public/img/icons/timer.svg';
-import IconTriangleGray from '../../../public/img/icons/triangle-gray.svg';
-import IconTriangleWhite from '../../../public/img/icons/triangle-white.svg';
+import IconAdd from '~public/img/icons/add.svg';
+import IconArrow from '~public/img/icons/arrow.svg';
+import IconCalendar from '~public/img/icons/calendar.svg';
+import IconCheckCircle from '~public/img/icons/check-circle.svg';
+import IconClose from '~public/img/icons/close.svg';
+import IconGraph from '~public/img/icons/graph.svg';
+import IconSearch from '~public/img/icons/search.svg';
+import IconSetting from '~public/img/icons/setting.svg';
+import IconSpinner from '~public/img/icons/spinner.svg';
+import IconTag from '~public/img/icons/tag.svg';
+import IconTime from '~public/img/icons/time.svg';
+import IconTimer from '~public/img/icons/timer.svg';
+import IconTriangleGray from '~public/img/icons/triangle-gray.svg';
+import IconTriangleWhite from '~public/img/icons/triangle-white.svg';
 
 @Component({
   components: {
--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,7 @@ module.exports = {
     resolve: {
       alias: {
         '~': path.join(__dirname, '/src/components'),
+        '~public': path.join(__dirname, '/public'),
       },
     },
   },
