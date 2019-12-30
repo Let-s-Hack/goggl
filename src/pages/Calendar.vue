cb1521d23a1a4211192bc03e609dc8f61be8930f
usmysa <b1012206@gmail.com>
2019-12-31 03:22:48 +0900

    #15 コンテント部分の実装

--- src/pages/Calendar.vue
+++ src/pages/Calendar.vue
@@ -7,41 +7,60 @@
         <SvgIcon class="Calendar_Setting" name="setting" />
       </div>
       <ul class="Calendar_DayWeekList">
-        <li class="Calendar_DayWeek">M</li>
-        <li class="Calendar_DayWeek">T</li>
-        <li class="Calendar_DayWeek">W</li>
-        <li class="Calendar_DayWeek">T</li>
-        <li class="Calendar_DayWeek">F</li>
-        <li class="Calendar_DayWeek">S</li>
-        <li class="Calendar_DayWeek">S</li>
+        <li
+          v-for="(dayWeek, index) in dayWeeks"
+          :key="index"
+          class="Calendar_DayWeek"
+        >
+          {{ dayWeek }}
+        </li>
       </ul>
       <ul class="Calendar_DayList">
         <li class="Calendar_Day">23</li>
         <li class="Calendar_Day">24</li>
         <li class="Calendar_Day">25</li>
         <li class="Calendar_Day">26</li>
-        <li class="Calendar_Day">27</li>
-        <li class="Calendar_Day">28</li>
-        <li class="Calendar_Day _isToday">29</li>
+        <li class="Calendar_Day _isToday">27</li>
+        <li class="Calendar_Day _isFuture">28</li>
+        <li class="Calendar_Day _isFuture">29</li>
       </ul>
     </div>
     <BaseContent class="Calendar_Content">
+      <ul class="Calendar_List">
+        <li
+          v-for="(time, index) in times"
+          :key="index"
+          class="Calendar_Item"
+        >
+          <span class="Calendar_Time">{{ time }}</span>
+          <hr class="Calendar_Line">
+        </li>
+
+      </ul>
     </BaseContent>
     <GlobalNav class="Calendar_Nav" />
   </div>
 </template>
 
 <script>
+import { Component, Vue } from 'vue-property-decorator';
 import BaseContent from '~/organisms/BaseContent.vue';
 import GlobalNav from '~/organisms/GlobalNav.vue';
 
-export default {
-  name: 'calendar',
+@Component({
   components: {
     BaseContent,
     GlobalNav,
   },
-};
+})
+export default class Calendar extends Vue {
+  dayWeeks = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+
+  times = [
+    '12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM',
+    '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM',
+  ];
+}
 </script>
 
 <style lang="scss" scoped>
@@ -78,7 +97,7 @@ export default {
     left: 0;
     right: 0;
     font-size: 1.4rem;
-    font-weight: 500;
+    font-weight: 400;
     text-align: center;
   }
 
@@ -128,6 +147,10 @@ export default {
       border-radius: 50%;
       line-height: 32px;
     }
+
+    &._isFuture {
+      color: #B5BCC0;
+    }
   }
 
   &_Content {
@@ -138,7 +161,37 @@ export default {
     background-color: #FAFAFC;
   }
 
+  &_List {
+    padding: 0 36px;
+    box-sizing: border-box;
+  }
+
+  &_Item {
+    display: flex;
+    align-items: center;
+    margin-top: 8px;
+    margin-bottom: 40px;
+
+    & + & {
+      margin-top: 48px;
+    }
+  }
+
+  &_Time {
+    width: 45px;
+    margin-right: 28px;
+    font-size: 1.2rem;
+    color: #C6CBCF;
+    white-space: nowrap;
+  }
+
+  &_Line {
+    width: 100%;
+    border-color: #C2C8CB;
+  }
+
   &_Nav {
+    z-index: $zIndex_calendarNav;
     box-shadow: 0 -2px 8px 0px rgba(#424147, 0.17);
   }
 }
