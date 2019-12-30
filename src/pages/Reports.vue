ffecb356308cb7457361aa6ddb2007d089299912
usmysa <b1012206@gmail.com>
2019-12-30 22:14:44 +0900

    #17 位置調整

--- src/pages/Reports.vue
+++ src/pages/Reports.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="Reports">
-    <ReportsHeader />
+    <ReportsHeader class="Reports_Header" />
     <BaseContent class="Reports_Content">
-      <ReportsSummary />
-      <ReportsBarGraph />
+      <div class="Reports_CardContainer">
+        <ReportsSummary class="Reports_Card" />
+        <ReportsBarGraph class="Reports_Card" />
+      </div>
       <template v-if="true">
-        <ReportsPieChart />
+        <ReportsPieChart class="Reports_PieChart"/>
       </template>
       <template v-else>
         <div class="Reports_NoReports">
@@ -17,12 +19,14 @@
         </div>
       </template>
     </BaseContent>
+    <GlobalNav class="Reports_Nav" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import BaseContent from '~/organisms/BaseContent.vue';
+import GlobalNav from '~/organisms/GlobalNav.vue';
 import ReportsBarGraph from '~/organisms/ReportsBarGraph.vue';
 import ReportsHeader from '~/organisms/ReportsHeader.vue';
 import ReportsPieChart from '~/organisms/ReportsPieChart.vue';
@@ -31,6 +35,7 @@ import ReportsSummary from '~/molecules/ReportsSummary.vue';
 @Component({
   components: {
     BaseContent,
+    GlobalNav,
     ReportsBarGraph,
     ReportsHeader,
     ReportsPieChart,
@@ -43,8 +48,35 @@ export default class Reports extends Vue {
 
 <style lang="scss" scoped>
 .Reports {
-  &_Content {
+  &_Header {
+    position: fixed;
+    top: 0;
+    width: 100%;
+    z-index: $zIndex_header;
+  }
 
+  &_Content {
+    position: absolute;
+    top: 0;
+    margin-top: 44px;
+    margin-bottom: 50px;
+  }
+
+  &_CardContainer {
+    padding: 0 16px;
+    box-sizing: border-box;
+  }
+
+  &_Card {
+    margin-top: 12px;
+
+    & + & {
+      margin-top: 24px;
+    }
+  }
+
+  &_PieChart {
+    margin-top: 32px;
   }
 
   &_Reports_NoReports {
@@ -56,6 +88,12 @@ export default class Reports extends Vue {
     &Text {
 
     }
+  }
+
+  &_Nav {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
   }
 }
 </style>
