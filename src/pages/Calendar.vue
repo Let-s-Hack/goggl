41378e4b0d17dc49ca2af172e7eb0f85506afb0b
usmysa <b1012206@gmail.com>
2019-12-31 13:57:39 +0900

    #15 レビュー修正

--- src/pages/Calendar.vue
+++ src/pages/Calendar.vue
@@ -44,7 +44,7 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import BaseContent from '~/organisms/BaseContent.vue';
 import GlobalNav from '~/organisms/GlobalNav.vue';
@@ -56,9 +56,9 @@ import GlobalNav from '~/organisms/GlobalNav.vue';
   },
 })
 export default class Calendar extends Vue {
-  dayWeeks = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+  dayWeeks: string[] = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
 
-  times = [
+  times: string[] = [
     '12 AM', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM',
     '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM',
   ];
