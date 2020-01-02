936647d1d10a0eb19fc98360acc297baf9dec9d7
usmysa <b1012206@gmail.com>
2020-01-03 01:45:24 +0900

    #114 storeの修正

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import firestore from '@/firestore';
+import { IBottomSheetBehaviorState } from './modules/bottomSheetBehavior';
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
-});
+export interface IRootState {
+  bottomSheetBehavior: IBottomSheetBehaviorState,
+}
+
+export default new Vuex.Store<IRootState>({});
