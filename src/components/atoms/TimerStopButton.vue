a51ead8dbc33340bcd9a686498dfe1d630640c1e
Homma <ab13891012.gmail.com>
2020-01-07 22:04:15 +0900

    #106 レビュー修正

--- src/components/atoms/TimerStopButton.vue
+++ src/components/atoms/TimerStopButton.vue
@@ -6,11 +6,7 @@
 </template>
 
 <script lang="ts">
-import {
-  Component,
-  Vue,
-  Prop,
-} from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
 
 @Component
 export default class TimerStopButton extends Vue {
