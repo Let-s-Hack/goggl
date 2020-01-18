5975bd581e1b9c2914fcc5ac94f2da16ee891b56
usmysa <b1012206@gmail.com>
2020-01-18 19:39:26 +0900

    #167 moduleの作成

---
+++ src/store/modules/ReportManager.ts
@@ -0,0 +1,66 @@
+import {
+  Module,
+  VuexModule,
+  Mutation,
+  Action,
+  getModule,
+} from 'vuex-module-decorators';
+import {
+  IReportState,
+  IReportManagerState,
+} from '@/store/types';
+import store from '@/store';
+
+// TODO: 一時的
+const tmpState = {
+  startDate: '2019-12-21',
+  endDate: '2019-12-27',
+  total: 19080,
+  billable: 0,
+  barGraph: {
+    '2019-12-21': 4680,
+    '2019-12-22': 4680,
+    '2019-12-23': 3240,
+    '2019-12-24': 3600,
+    '2019-12-25': 1800,
+    '2019-12-26': 0,
+    '2019-12-27': 1080,
+  },
+  pieChart: {
+    Finance: 11520,
+    goggl: 4680,
+    LINE: 1800,
+    CMS: 1080,
+  },
+};
+
+@Module({
+  name: 'ReportManager',
+  namespaced: true,
+  dynamic: true,
+  store,
+})
+class ReportManager extends VuexModule implements IReportManagerState {
+  public reportState: IReportState = { ...tmpState };
+
+  @Mutation
+  public setState(payload: IReportState) {
+    this.reportState = payload;
+  }
+
+  @Action
+  public fetch(): boolean {
+    // TODO: firebase処理
+    const isSuccess = true;
+    if (isSuccess) {
+      this.context.commit('setState', tmpState);
+      return true;
+    }
+
+    return false;
+  }
+}
+
+const reportManager = getModule(ReportManager);
+
+export default reportManager;
