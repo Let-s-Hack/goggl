cfe9d862e44a97c46b41973ba157e3a319635257
Homma <ab13891012.gmail.com>
2020-01-19 22:04:42 +0900

    #171 レコードグループのロジック作成

--- src/components/organisms/RecordGroup.vue
+++ src/components/organisms/RecordGroup.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="RecordGroup">
-    <RecordGroupHeader class="RecordGroup_Header" />
-    <ul>
-      <Record @click.native="showTimerEditor()" class="RecordGroup_Item" />
-      <RecordList class="RecordGroup_Item" />
+    <RecordGroupHeader class="RecordGroup_Header">
+      <template v-slot:date>{{ displayDate }}</template>
+      <template v-slot:total>{{ displayTotal }}</template>
+    </RecordGroupHeader>
+    <ul
+      v-for="(records, index) in getRecordGroup"
+      :key="index"
+    >
+      <Record
+        v-if="records.length === 1"
+        @click.native="showTimerEditor()"
+        :record="records[0]"
+        class="RecordGroup_Item"
+      />
+      <RecordList
+        v-else
+        :records="records"
+        class="RecordGroup_Item"
+      />
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { reduce, isEqual } from 'lodash';
+import moment, { Moment } from 'moment';
+import { ITimerState } from '@/store/types';
 import PageLayer from '@/store/modules/PageLayer';
 import Record from '~/atoms/Record.vue';
+import { IRecordGroup } from '~/organisms/RecordContainer.vue';
 import RecordList from '~/molecules/RecordList.vue';
 import RecordGroupHeader from '~/molecules/RecordGroupHeader.vue';
 import TimerEditor from '~/organisms/TimerEditor.vue';
@@ -24,10 +43,79 @@ import TimerEditor from '~/organisms/TimerEditor.vue';
   },
 })
 export default class RecordGroup extends Vue {
+  @Prop({ required: true }) recordGroup!: IRecordGroup;
+
   private pageLayer = PageLayer;
 
   private showTimerEditor(): void {
     this.pageLayer.push({ component: TimerEditor });
+  }
+
+  mounted() {
+    console.log('propRecordGroup', this.getRecordGroup);
+  }
+
+  private get displayDate(): string {
+    const date: Moment = moment(this.recordGroup.date);
+
+    if (date.isSame(moment(), 'day')) return 'Today';
+    if (date.isSame(moment().subtract(1, 'day'), 'day')) return 'Yesterday';
+
+    return date.format('ddd, DD MMM');
+  }
+
+  private get displayTotal(): string {
+    let total = 0;
+
+    this.recordGroup.records.forEach((record: ITimerState) => {
+      if (record.startDatetime === null || record.endDatetime === null) return;
+
+      const start: Moment = moment(record.startDatetime);
+      const end: Moment = moment(record.endDatetime);
+
+      // ミリ秒から秒にして合計値に追加する
+      total += Math.floor(end.diff(start) / 1000);
+    });
+
+    const hours: number = Math.floor(total / 3600);
+    const minutes: number = Math.floor((total - (hours * 3600)) / 60);
+    const seconds: number = total % 100;
+    const displayTotal: string = (
+      `${hours}:`
+      + `${minutes < 10 ? '0' : ''}${minutes}:`
+      + `${seconds < 10 ? '0' : ''}${seconds}`
+    );
+
+    return displayTotal;
+  }
+
+  private get getRecordGroup(): ITimerState[][] {
+    let nextRecordGroup: (
+      ITimerState[]
+      | ITimerState[][]
+    ) = this.recordGroup.records;
+
+    nextRecordGroup = reduce(nextRecordGroup, (
+      result: ITimerState[][],
+      value: ITimerState,
+    ): ITimerState[][] => {
+      // 名前、プロジェクトID、タグIDが同じレコードの配列番号を求める
+      const dateIndex: number = result.findIndex((element: ITimerState[]) => (
+        element[0].name === value.name
+        && element[0].projectId === value.projectId
+        && isEqual(element[0].tagIds, value.tagIds)
+      ));
+
+      if (dateIndex >= 0) {
+        result[dateIndex].push(value);
+        return result;
+      }
+
+      result.push([value]);
+      return result;
+    }, []);
+
+    return nextRecordGroup;
   }
 }
 </script>
