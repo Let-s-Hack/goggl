a5ebf49214b63ee972279ef4afc2ac69067e3e81
Homma <ab13891012.gmail.com>
2020-01-19 22:04:03 +0900

    #171 RecordContainerのロジック作成

--- src/components/organisms/RecordContainer.vue
+++ src/components/organisms/RecordContainer.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="RecordContainer">
-    <RecordGroup />
-    <RecordGroup />
-    <RecordGroup />
+  <div>
+    <div class="RecordContainer">
+      <RecordGroup
+        v-for="recordGroup in getRecordGroupList"
+        :key="recordGroup.date"
+        :recordGroup="recordGroup"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { orderBy, reduce } from 'lodash';
+import moment from 'moment';
+import { ITimerState } from '@/store/types';
+import RecordManager from '@/store/modules/RecordManager';
 import RecordGroup from '~/organisms/RecordGroup.vue';
+
+export interface IRecordGroup {
+  date: string,
+  records: ITimerState[],
+}
 
 @Component({
   components: {
@@ -16,5 +29,38 @@ import RecordGroup from '~/organisms/RecordGroup.vue';
   },
 })
 export default class RecordContainer extends Vue {
+  recordManager = RecordManager;
+
+  created() {
+    this.recordManager.fetch();
+  }
+
+  private get getRecordGroupList(): IRecordGroup[] {
+    let nextRecordGroupList: (
+      ITimerState[]
+      | IRecordGroup[]
+    ) = this.recordManager.recordState;
+
+    nextRecordGroupList = orderBy(nextRecordGroupList, ['startDatetime'], ['desc']);
+    nextRecordGroupList = reduce(nextRecordGroupList, (
+      result: IRecordGroup[],
+      value: ITimerState,
+    ): IRecordGroup[] => {
+      const date: string = moment(value.startDatetime!).format('YYYY-MM-DD');
+      const dateIndex: number = result.findIndex((element: IRecordGroup) => (
+        element.date === date
+      ));
+
+      if (dateIndex >= 0) {
+        result[dateIndex].records.push(value);
+        return result;
+      }
+
+      result.push({ date, records: [value] });
+      return result;
+    }, []);
+
+    return nextRecordGroupList;
+  }
 }
 </script>
