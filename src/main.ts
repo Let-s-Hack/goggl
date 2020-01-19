166d17bba5e1c5b4623e4e9a70d59936d621338a
usmysa <b1012206@gmail.com>
2020-01-19 13:57:59 +0900

    #178 変換処理の実装

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,15 @@ import '@/assets/sass/main.scss';
 import SvgIcon from '~/atoms/SvgIcon.vue';
 
 Vue.filter('format', (value: string, format: string): string => moment(value).format(format));
+Vue.filter('toTime', (value: number): string => {
+  if (value <= 0) return '0:00:00';
+
+  const hours: number = Math.floor(value / 3600);
+  const minutes: number = Math.floor((value - hours * 3600) / 60);
+  const seconds: number = Math.floor(value - hours * 3600 - minutes * 60);
+
+  return `${hours}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+});
 
 Vue.config.productionTip = false;
 
