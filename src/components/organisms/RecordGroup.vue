8753338917dfe2daf1024ab4fa5eb788038d3c65
Homma <ab13891012.gmail.com>
2020-01-26 00:00:39 +0900

    #171 RecordGroup表示ロジック作成

--- src/components/organisms/RecordGroup.vue
+++ src/components/organisms/RecordGroup.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="RecordGroup">
-    <RecordGroupHeader class="RecordGroup_Header" />
-    <ul>
-      <Record @click.native="showTimerEditor()" class="RecordGroup_Item" />
-      <RecordList class="RecordGroup_Item" />
+    <RecordGroupHeader class="RecordGroup_Header">
+      <template v-slot:date>
+        <template v-if="diffDays() > 1">
+          {{ recordGroup.date | format('ddd, DD MMM') }}
+        </template>
+        <template v-else-if="diffDays() === 1">
+          Yesterday
+        </template>
+        <template v-else>
+          Today
+        </template>
+      </template>
+      <template v-slot:total>{{ totalSeconds | toTime }}</template>
+    </RecordGroupHeader>
+    <ul
+      v-for="(records, index) in recordsList"
+      :key="records[0].id || index"
+    >
+      <RecordList
+        v-if="records.length > 1"
+        :records="records"
+        class="RecordGroup_Item"
+      />
+      <Record
+        v-else-if="records[0]"
+        @click.native="showTimerEditor()"
+        :record="records[0]"
+        class="RecordGroup_Item"
+      />
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { findIndex, forEach } from 'lodash';
+import moment, { Moment } from 'moment';
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import { IRecordGroup } from '~/types';
+import { ITimerState } from '@/store/types';
 import PageLayer from '@/store/modules/PageLayer';
 import Record from '~/atoms/Record.vue';
 import RecordList from '~/molecules/RecordList.vue';
@@ -24,10 +53,56 @@ import TimerEditor from '~/organisms/TimerEditor.vue';
   },
 })
 export default class RecordGroup extends Vue {
+  @Prop({ required: true }) recordGroup!: IRecordGroup;
+
   private pageLayer = PageLayer;
 
   private showTimerEditor(): void {
     this.pageLayer.push({ component: TimerEditor });
+  }
+
+  private diffDays(): number {
+    const now: Moment = moment();
+    const recordDate: Moment = moment(this.recordGroup.date);
+    const diffDays: number = now.diff(recordDate, 'days');
+
+    return diffDays;
+  }
+
+  private get recordsList(): ITimerState[][] {
+    const recordsList: ITimerState[][] = [];
+
+    forEach(this.recordGroup.records, (record: ITimerState): void => {
+      const sameRecordIndex: number = findIndex(recordsList, [0, {
+        title: record.title,
+        projectId: record.projectId,
+        tagIds: record.tagIds,
+      }]);
+
+      if (sameRecordIndex >= 0) {
+        recordsList[sameRecordIndex].push(record);
+        return;
+      }
+
+      recordsList.push([record]);
+    });
+
+    return recordsList;
+  }
+
+  private get totalSeconds(): number {
+    let totalSeconds = 0;
+
+    forEach(this.recordGroup.records, (record: ITimerState): void => {
+      if (record.startDatetime === null || record.endDatetime === null) return;
+
+      const start: Moment = moment(record.startDatetime);
+      const end: Moment = moment(record.endDatetime);
+
+      totalSeconds += Math.floor(end.diff(start, 'seconds'));
+    });
+
+    return totalSeconds;
   }
 }
 </script>
