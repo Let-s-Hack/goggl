03134fe0a97e165ac561296ea2e611ba172bf138
Homma <ab13891012.gmail.com>
2020-02-08 17:58:11 +0900

    #171 Record, RecordListコンポーネントを作成

--- src/components/atoms/RecordListItem.vue
+++ src/components/atoms/RecordListItem.vue
@@ -1,31 +1,60 @@
 <template>
   <li class="RecordListItem">
     <div class="RecordListItem_Left">
-      <!-- TODO: _isEmptyの出し分け -->
-      <h4 class="RecordListItem_Title">タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル</h4>
-      <!-- colorとborder-colorにstyle属性でカラーコードを指定する -->
+      <h4
+        :class="['RecordListItem_Title', { '_isEmpty': !record.title }]"
+      >{{ record.title || 'Add description' }}</h4>
       <span
+        v-if="!recordManager.isNoProject(record.projectId)"
         class="RecordListItem_Project"
-        :style="{ borderColor: '#3F46E3', color: '#3F46E3' }"
-      >
-        ゴーグル
-      </span>
+        :style="{
+          borderColor: project.color,
+          color: project.color
+        }"
+      >{{ project.name }}</span>
     </div>
-    <div class="RecordListItem_Center">
+    <div
+      v-if="recordManager.existsTags(record.id)"
+      class="RecordListItem_Center"
+    >
       <SvgIcon name="tag" class="RecordListItem_IconTag" />
     </div>
     <div class="RecordListItem_Right">
-      <span class="RecordListItem_Duration">0:30:00</span>
+      <span class="RecordListItem_Duration">{{ duration | toTime }}</span>
       <SvgIcon name="triangle" class="RecordListItem_IconStart" />
     </div>
   </li>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import moment, { Moment } from 'moment';
+import {
+  Component,
+  Prop,
+  Vue,
+} from 'vue-property-decorator';
+import {
+  IProjectState,
+  ITimerState,
+} from '@/store/types';
+import ProjectManager from '@/store/modules/ProjectManager';
+import RecordManager from '@/store/modules/RecordManager';
 
 @Component
 export default class RecordListItem extends Vue {
+  @Prop({ required: true }) record!: ITimerState;
+
+  private projectManager = ProjectManager;
+
+  private recordManager = RecordManager;
+
+  private get project(): IProjectState | undefined {
+    return this.projectManager.getById(this.record.projectId);
+  }
+
+  private get duration(): number {
+    return this.recordManager.getDurationById(this.record.id);
+  }
 }
 </script>
 
